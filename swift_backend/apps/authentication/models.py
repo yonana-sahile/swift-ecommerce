a0bb2ae7542ca54f4d3fd78b5b6d9a80a57277d1
Yonas Sahile <yonassahile8@gmail.com>
2026-07-09 12:31:01 +0300

    feat(auth): add custom User model with email authentication

--- swift_backend/apps/authentication/models.py
+++ swift_backend/apps/authentication/models.py
@@ -1,3 +1,12 @@
+# swift_backend/authentication/models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
